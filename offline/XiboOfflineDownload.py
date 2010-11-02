34260783ec76d682108e454155d24895a2f03468
Alex Harrington <alex@longhill.org.uk>
2010-11-02 22:45:03 +0000

    [offline] Improved logging. Added displays now written to configuration. Config edit/save now working inside application.

--- offline/XiboOfflineDownload.py
+++ offline/XiboOfflineDownload.py
@@ -45,7 +45,7 @@ class XiboOfflineDownload(XiboOfflineDownloadUI):
     # Called after the GUI is initialised but before it's shown
     def setup_tasks(self):
         self.SetTitle(APP_NAME)
-        self.txtOutput.AppendText('%s v%s' % (APP_NAME,VERSION));
+        self.txtOutput.AppendText('%s v%s\n' % (APP_NAME,VERSION));
 
         # Define Variables
         self.AddDisplayDialog = None
@@ -53,6 +53,9 @@ class XiboOfflineDownload(XiboOfflineDownloadUI):
         # Figure out where our config is saved
         self.__config_path = os.path.expanduser('~')
         self.__config_file = os.path.join(self.__config_path,'xibo-offline.cfg')
+
+        global log
+        log = self.writeLog
 
         # Read config (if exists)
         global config
@@ -63,10 +66,15 @@ class XiboOfflineDownload(XiboOfflineDownloadUI):
         config.read([self.__config_file])
 
         global verbose
-        verbose = False
+        try:
+            verbose = config.getboolean('Main','verbose')
+        except ValueError:
+            config.set('Main','verbose','false')
+            self.saveConfig()
+            verbose = False
+
         # Setup the GUI with options from the config file
-        if config.get('Main','verbose') == 'true':
-            verbose = True
+        if verbose:
             self.chkVerbose.SetValue(True)
 
         if config.get('Main','xmdsUrl') != '':
@@ -80,11 +88,6 @@ class XiboOfflineDownload(XiboOfflineDownloadUI):
 
 
     def updateDisplays(self):
-        config.readfp(open('defaults.cfg'))
-
-        print _("Reading user configuration")
-        config.read([self.__config_file])
-
         # Add displays defined in the config file
         displayNames = []
 
@@ -93,12 +96,31 @@ class XiboOfflineDownload(XiboOfflineDownloadUI):
                 displayNames.append(section)
         # End For Loop
 
-        # Sort the names alphabetically
-        displayNames.sort()
+        # Sort the names alphabetically ignoring case
+        displayNames.sort(key=lambda x: x.lower())
 
         # Clear the list
         self.selectedDisplays.Set(displayNames)
 
+    def writeLog(self,message,appendLineBreak=True,important=False):
+        if verbose or important:
+            self.txtOutput.AppendText(message)
+            if appendLineBreak:
+                self.txtOutput.AppendText('\n')
+
+    def getConfigFile(self):
+        return self.__config_file
+
+    def saveConfig(self):
+        try:
+            fh = open(self.getConfigFile(), 'w+')
+            config.write(fh)
+            fh.close()
+        except IOError:
+            log('Unable to write configuration file at %s. Check your filesystem permissions.' % self.__parent.getConfigFile(),True,True)
+            return False
+
+        return True
 
     # Event Handlers
     def onSelectAll(self, event): # wxGlade: XiboOfflineDownloadUI.<event_handler>
@@ -106,8 +128,10 @@ class XiboOfflineDownload(XiboOfflineDownloadUI):
         numItems = self.selectedDisplays.GetCount()
         for i in range(0,numItems):
             self.selectedDisplays.SetSelection(i)
+        
+        if numItems > 0:
+            self.btnRemove.Enable()
 
-        self.btnRemove.Enable()
         event.Skip()
 
     def onSelectNone(self, event): # wxGlade: XiboOfflineDownloadUI.<event_handler>
@@ -158,7 +182,20 @@ class XiboOfflineDownload(XiboOfflineDownloadUI):
         event.Skip()
 
     def onConfigSave(self, event): # wxGlade: XiboOfflineDownloadUI.<event_handler>
-        print "Event handler `onConfigSave' not implemented!"
+        config.set('Main','xmdsUrl',self.txtServerURL.GetValue())
+        config.set('Main','xmdsKey',self.txtServerKey.GetValue())
+
+        if self.chkVerbose.IsChecked():
+            config.set('Main','verbose','true')
+        else:
+            config.set('Main','verbose','false')
+
+        if not self.saveConfig():
+            event.Skip()
+            return
+
+        log('Configuration saved',True,True)
+        self.btnSave.Disable()
         event.Skip()
 
     def onDisplayListDClick(self, event): # wxGlade: XiboOfflineDownloadUI.<event_handler>
@@ -186,12 +223,35 @@ class XiboOfflineDownload(XiboOfflineDownloadUI):
 
 class AddDisplay(AddDisplayUI):
     def onCreateDisplay(self, event): # wxGlade: AddDisplayUI.<event_handler>
+        # Save the new display to config
+        try:
+            config.add_section(self.txtClientName.GetValue())
+        except ConfigParser.DuplicateSectionError:
+            log('A display with that name already exists.',True,True)
+            event.Skip()
+            return
+        except ValueError:
+            log('You may not name a display "DEFAULT". Please choose another name.',True,True)
+            event.Skip()
+            return
+
+        try:
+            config.set(self.txtClientName.GetValue(),'license',self.txtClientKey.GetValue())
+        except ConfigParser.NoSectionError:
+            log('Something went wrong adding the display to your configuration file.',True,True)
+            event.Skip()
+            return
+
+        if not self.__parent.saveConfig():
+            event.Skip()
+            return
+
         # Actually Add the Display
         xmds = XMDS(self.txtClientKey.GetValue(),self.txtClientName.GetValue(),config.get('Main','xmdsKey'),self.__parent.txtOutput)
         try:
             xmds.RegisterDisplay()
         except XMDSException:
-            print "Webservice Exception"
+            log('Webservice Exception',True,True)
             event.Skip()
             return
 
